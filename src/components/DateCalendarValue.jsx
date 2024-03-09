6c9df8547b726b7f50ea3ec7b30d8d441caefbc8
em-il-yn <63993946+em-il-yn@users.noreply.github.com>
2024-03-09 15:03:01 -0800

    Calendar Added

---
+++ src/components/DateCalendarValue.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+export default function FirstComponent() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar />
+    </LocalizationProvider>
+  );
+}
